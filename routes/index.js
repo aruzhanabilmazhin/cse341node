4d432f318f5a49481b9c6779e3d49d30488a5b99
aruzhana <aruzhana@byui.edu>
2026-01-11 21:24:37 +0500

    nnnn

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 import express from 'express';
+import contactsRouter from './contacts.js'; // make sure this file exists
+
 const router = express.Router();
 
+// Mount contacts routes at /contacts
+router.use('/contacts', contactsRouter);
+
+// Optional: home route
 router.get('/', (req, res) => {
-  res.send('API is running');
+  res.send('Welcome to the Contacts API');
 });
 
 export default router;
